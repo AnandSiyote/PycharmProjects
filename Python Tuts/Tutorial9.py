46d3fcc5be81027623c39da5f414a3c9b7bd3796
Anand Siyote <aanandsiyote@gmail.com>
2019-03-24 18:49:58 +0530

    Tutorials from Code with Harry

---
+++ Python Tuts/Tutorial9.py
@@ -0,0 +1,15 @@
+mystr = "Harry is a good boy"
+# print(len(mystr))
+# print(mystr[::-2])
+
+print(mystr.endswith("bdoy"))
+print(mystr.count("o"))
+print(mystr.capitalize())
+print(mystr.replace("is", "are"))
+
+print(mystr.isalnum())
+print(mystr.isalpha())
+print(mystr.find("is"))
+print(mystr.lower())
+print(mystr.upper())
+print(mystr.replace("is","are"))
